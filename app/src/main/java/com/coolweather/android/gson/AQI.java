7c0f237eb81a8159d2d015b5cbc385b1f738e87f
YJX666 <yjx_1206@163.com>
2019-04-13 11:09:41 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by YJX on 2019/4/11.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
